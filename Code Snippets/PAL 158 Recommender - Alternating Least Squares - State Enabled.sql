2e15de442f440a77624b8b1e176e4590d458159f
SAP HANA Academy <hanaacademy@sap.com>
2018-04-23 13:50:49 +0200

    SAP HANA 2.0 SPS03
    SAP HANA 2.0 SPS03

--- Code Snippets/PAL 158 Recommender - Alternating Least Squares - State Enabled.sql
+++ Code Snippets/PAL 158 Recommender - Alternating Least Squares - State Enabled.sql
@@ -11,46 +11,41 @@ CREATE COLUMN TABLE "State" ("name" VARCHAR(50), "value" VARCHAR(100));
 INSERT INTO "#Params" VALUES ('ALGORITHM', 24, null, null); -- 1: SVM, 2: Random DT, 3: Decision Tree, 4: Cluster Assignment, 5: LDA Inference, 6: Binning, 7: Naive Bayes, 8: PCA, 9: BPNN, 11: FRM, 16: Multiple Linear Regression, 20: Logistic Regression, 24: ALS
 INSERT INTO "#Params" VALUES ('STATE_DESCRIPTION', null, null, 'My Trained ALS Model');
 
-CALL "_SYS_AFL"."CREATE_PAL_MODEL_STATE" ("ModelMetadata", "ModelMap", "ModelFactors", "#Empty", "#Empty", "#Params", "State") WITH OVERVIEW;
+CALL "_SYS_AFL"."PAL_CREATE_MODEL_STATE" ("ModelMetadata", "ModelMap", "ModelFactors", "#Empty", "#Empty", "#Params", "State") WITH OVERVIEW;
 
 SELECT * FROM "State";
+
 SELECT * FROM "SYS"."M_AFL_STATES";
-SELECT * FROM "SYS"."M_AFL_FUNCTIONS";
 
 
 -- predict with state
 
 DROP TABLE "RatingsNew";
-DROP TABLE "#Params";
-DROP TABLE "Predictions";
+DROP TABLE "EmptyModelMetadata";
+DROP TABLE "EmptyModelMap";
+DROP TABLE "EmptyModelFactors";
+TRUNCATE TABLE "#Params";
 
 CREATE COLUMN TABLE "RatingsNew" ("id" INTEGER, "user" NVARCHAR(255), "album" NVARCHAR(255));
-CREATE LOCAL TEMPORARY COLUMN TABLE "#Params" ("name" VARCHAR(60), "intArgs" INTEGER, "doubleArgs" DOUBLE, "stringArgs" VARCHAR(100));
-CREATE COLUMN TABLE "Predictions" ("id" INTEGER, "user" NVARCHAR(255), "album" NVARCHAR(255), "predictedRating" INTEGER);
-
-INSERT INTO "RatingsNew" VALUES (1,'Julie','Led Zeppelin IV');
-INSERT INTO "RatingsNew" VALUES (2,'Jamie','Born in the U.S.A.');
+CREATE COLUMN TABLE "EmptyModelMetadata" LIKE "ModelMetadata";
+CREATE COLUMN TABLE "EmptyModelMap" LIKE "ModelMap";
+CREATE COLUMN TABLE "EmptyModelFactors" LIKE "ModelFactors";
 
 INSERT INTO "#Params" ("name", "stringArgs") SELECT "name", "value" FROM "State";
 
-CALL "_SYS_AFL"."PAL_ALS_PREDICT" ("RatingsNew", "ModelMetadata", "ModelMap", "ModelFactors", "#Params", "Predictions") WITH OVERVIEW;
+TRUNCATE TABLE "RatingsNew";
+INSERT INTO "RatingsNew" VALUES (1,'Julie','Led Zeppelin IV');
+--INSERT INTO "RatingsNew" VALUES (1,'Jamie','Born in the U.S.A.');
 
-SELECT * FROM "Predictions";
-SELECT * FROM "SYS"."M_AFL_STATES";
+CALL "_SYS_AFL"."PAL_ALS_PREDICT" ("RatingsNew", "EmptyModelMetadata", "EmptyModelMap", "EmptyModelFactors", "#Params", ?);
 
 
 -- delete state
 
-DROP TABLE "#Params";
-DROP TABLE "Messages";
+TRUNCATE TABLE "#Params";
 
-CREATE LOCAL TEMPORARY COLUMN TABLE "#Params" ("name" VARCHAR(60), "intArgs" INTEGER, "doubleArgs" DOUBLE, "stringArgs" VARCHAR(100));
-CREATE COLUMN TABLE "Messages" ("id" VARCHAR(50), "timeStamp" VARCHAR(100), "errorCode" INTEGER, "message" VARCHAR(200));
-
-CALL "_SYS_AFL"."DELETE_PAL_MODEL_STATE" ("State", "#Params", "Messages") WITH OVERVIEW;
+CALL "_SYS_AFL"."PAL_DELETE_MODEL_STATE" ("State", "#Params", ?);
 
 TRUNCATE TABLE "State";
 
-SELECT * FROM "Messages";
 SELECT * FROM "SYS"."M_AFL_STATES";
-SELECT * FROM "SYS"."M_AFL_FUNCTIONS";
